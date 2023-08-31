3b40e1e852543cff82ed7f23b3717305210ef758
nickolay.grin <nick@singularitysolutions.com>
2023-08-31 18:52:18 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -18,6 +18,13 @@ if ( $(this).scrollTop() > 0 && $menu.hasClass("default") ){
     $menu.removeClass("fixed").addClass("default");
   }
 
+$(".item-question__head").click(function() {
+		$(this).parent().toggleClass("active");
+		$(this).siblings().slideToggle(200);
+		$(".item-question__head").not(this).parent(".item-question").removeClass("active");
+		$(".item-question__head").not(this).siblings(".item-question__content").slideUp(200);
+	});
+
 	//плавный скролл
 	$(".navigat li a").mPageScroll2id();
 
